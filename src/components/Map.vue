a1dd84d7bce5f5fb65d3ab4ec91cdfbfcd4ad786
PaddyMurphy <patrickblewis@gmail.com>
2017-05-14 17:10:59 -0500

    map controls

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,10 +1,27 @@
+<!--
+TODO:
+- fetch usgs river basins
+- overlay rivers shp file
+- select river to focus on river and show details
+- assign descriptions to rivers
+- show recommended flow rates
+- indicate whitewater or fishing/camping
+-->
 <template>
-  <div id="map">
+  <div class="map">
+    <map-controls
+      :showKmlLayer="showKmlLayer"
+      v-on:toggleSatellite="toggleSatellite"
+      v-on:toggleKml="toggleKml"
+    />
 
+    <div id="gmap"></div>
   </div>
 </template>
 
 <script>
+import mapStyles from '@/components/mapStyles.json'
+import MapControls from '@/components/MapControls';
 import loadGoogleMapsAPI from 'load-google-maps-api';
 
 export default {
@@ -12,9 +29,15 @@ export default {
   data () {
     return {
       key: 'AIzaSyA1fV5RautETdjZsTMpClgqHKIgMa8WXPI',
-      center: {lat: 31.96, lng: -99.90},
+      center: {lat: 31.96, lng: -99.90}, // Texas
+      mapStyles: mapStyles.styles,
+      mapType: 'terrain',
       kmlLayer: 'http://waterwatch.usgs.gov/?m=real&w=kml&regions=tx',
-      zoom: 6 // TODO: lower for mobile
+      kmlData: undefined,
+      showKmlLayer: true,
+      zoom: 6, // TODO: lower for mobile
+      showKml: true,
+      showSatellite: false
     }
   },
   mounted: function () {
@@ -22,12 +45,39 @@ export default {
 
     vm.$once(vm.loadGoogleMaps());
   },
+  components: {
+    'map-controls': MapControls
+  },
   watch: {
     '$route' (to, from) {
       // Call resizePreserveCenter() on all maps
+    },
+    showKmlLayer: function () {
+      this.showKmlLayer ? this.displayKmlLayer() : this.hideKmlLayer();
     }
   },
   methods: {
+    toggleKml: function () {
+      // toggled from component
+      this.showKmlLayer = !this.showKmlLayer;
+    },
+    displayKmlLayer: function () {
+      var vm = this;
+      // eslint-disable-next-line
+      vm.kmlData = new window.google.maps.KmlLayer({
+        url: vm.kmlLayer,
+        map: window.gmap,
+        suppressInfoWindows: true,
+        preserveViewport: true
+      });
+    },
+    hideKmlLayer: function () {
+      this.kmlData.setMap(null);
+    },
+    toggleSatellite: function () {
+      console.log('toggleSatellite');
+      window.gmap.setMapTypeId('satellite');
+    },
     loadGoogleMaps: function () {
       const vm = this;
 
@@ -35,32 +85,35 @@ export default {
 
       loadGoogleMapsAPI({
         key: vm.key
-      }).then(function (googleMaps) {
-        vm.initMap(googleMaps);
+      }).then((googleMaps) => {
+        vm.initMap();
       }).catch((err) => {
         console.error(err)
       })
     },
-    initMap: function (googleMaps) {
+    initMap: function () {
       const vm = this;
-
-      var map = new googleMaps.Map(vm.$el, {
+      // save new gmap to window
+      window.gmap = new window.google.maps.Map(vm.$el.querySelector('#gmap'), {
         zoom: vm.zoom,
-        center: vm.center
+        center: vm.center,
+        styles: vm.mapStyles,
+        mapTypeId: vm.mapType
       });
 
-      var cta = new googleMaps.KmlLayer({ // eslint-disable-line
-        url: vm.kmlLayer,
-        map: map
-      });
-    }
+      // vm.setGoogleMapEvents();
+      if (vm.showKmlLayer) {
+        vm.displayKmlLayer();
+      }
+    },
+    setGoogleMapEvents: function () {}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#map {
+#gmap {
   width: 100vw;
   height: 80vh;
 }
---
+++ src/components/MapControls.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="map-controls">
+    <button
+      class="btn-kml"
+      @click.prevent="toggleKml"
+      >{{ buttonText }} Gauges</button>
+    <button class="btn-satellite" @click="toggleSatellite">Satellite</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {}
+  },
+  props: {
+    showKmlLayer: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    buttonText: function () {
+      return this.showKmlLayer ? 'Hide' : 'Show'
+    }
+  },
+  methods: {
+    toggleKml: function () {
+      this.$emit('toggleKml');
+    },
+    toggleSatellite: function () {
+      this.$emit('toggleSatellite');
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/scss/variables.scss';
+
+</style>
